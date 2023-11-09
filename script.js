99265996c1159fb3305916656559ea973d3ece84
messiribeiro <micaelribeirodossantos@gmail.com>
2023-11-09 00:47:07 -0300

    fixed script issue

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const video = document.querySelector('#vidPlayer')
-
-window.navigator.mediaDevices.getUserMedia({ video: true })
-.then(stream => {
-    video.srcObject = stream;
-    video.onloadedmetadata = (e) => {
-        video.play();
-    };
-})
-.catch( () => {
-    alert('You have give browser the permission to run Webcam and mic ;( ');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.querySelector('#vidPlayer');
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+        video.onloadedmetadata = (e) => {
+            video.play();
+        };
+    })
+    .catch( () => {
+        alert('You have to give the browser permission to run the webcam and microphone ;( ');
+    });
 });
