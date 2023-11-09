d4fce1f88a9415d1794b0130372ab3b747dc2095
messiribeiro <micaelribeirodossantos@gmail.com>
2023-11-09 00:24:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+window.navigator.mediaDevices.getUserMedia({ video: true })
+.then(stream => {
+    video.srcObject = stream;
+    video.onloadedmetadata = (e) => {
+        video.play();
+    };
+})
+.catch( () => {
+    alert('You have give browser the permission to run Webcam and mic ;( ');
+});
